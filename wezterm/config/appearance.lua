c7c3c687bbf96b568cd0aeb7df0d38209a059c8d
Zaid <azaidrahman@gmail.com>
2025-03-04 12:47:05 +0800

    wezterm
    fixed fps issue causing nvim to be laggy

--- wezterm/config/appearance.lua
+++ wezterm/config/appearance.lua
@@ -4,8 +4,8 @@ local colors = require("colors.custom")
 local mux = wezterm.mux
 
 return {
-	animation_fps = 60,
-	max_fps = 60,
+	animation_fps = 240,
+	max_fps = 240,
 	front_end = "WebGpu",
 	webgpu_power_preference = "HighPerformance",
 	webgpu_preferred_adapter = gpu_adapters:pick_best(),
@@ -32,8 +32,8 @@ return {
 
 	-- tab bar
 	enable_tab_bar = true,
-	hide_tab_bar_if_only_one_tab = false,
-	use_fancy_tab_bar = false,
+	hide_tab_bar_if_only_one_tab = true,
+	use_fancy_tab_bar = true,
 	tab_max_width = 25,
 	show_tab_index_in_tab_bar = false,
 	switch_to_last_active_tab_when_closing_tab = true,
