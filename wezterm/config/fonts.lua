911af9e75d4d6a93bfb21d99b372d29910b0112b
Zaid <azaidrahman@gmail.com>
2025-05-30 18:25:20 +0800

    added buffer stuff and fixed font error

--- wezterm/config/fonts.lua
+++ wezterm/config/fonts.lua
@@ -6,7 +6,7 @@ local font_family = 'JetBrainsMono Nerd Font'
 local font_size = platform.is_mac and 12 or 15
 
 return {
-   font = wezterm.font({
+   font = wezterm.font_with_fallback({
       family = font_family,
       weight = 'Medium',
    }),
@@ -15,4 +15,5 @@ return {
    --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
    freetype_load_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
    freetype_render_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
+   warn_about_missing_glyphs = false,
 }
