74f91729d4fd4cf22c0005f7262d15ec9d62aeec
Zaid <azaidrahman@gmail.com>
2025-06-19 10:17:14 +0800

    rainbow indents/delimiter

---
+++ nvim/lua/zaid/plugins/indent.lua
@@ -0,0 +1,38 @@
+-- https://github.com/lukas-reineke/indent-blankline.nvim?tab=readme-ov-file
+local highlight_list = {
+	"RainbowRed",
+	"RainbowYellow",
+	"RainbowBlue",
+	"RainbowOrange",
+	"RainbowGreen",
+	"RainbowViolet",
+	"RainbowCyan",
+}
+return {
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		event = { "BufReadPost", "BufNewFile" },
+		opts = {
+			indent = {
+				char = "|",
+				highlight = highlight_list,
+			},
+		},
+		config = function(_, opts)
+			local hooks = require("ibl.hooks")
+			hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+				vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+				vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+				vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+				vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+				vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+				vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+				vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+			end)
+			-- paste the hooks code here
+			-- change the setup() call to:
+			require("ibl").setup(opts)
+		end,
+	},
+}
